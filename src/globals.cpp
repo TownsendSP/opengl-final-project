3f62ebc87392ec5170da2f36334cc35602a507c5
TownsendSP <tgsoutha@syr.edu>
2024-04-30 17:09:38 -0400

    textures are broken again

--- src/globals.cpp
+++ src/globals.cpp
@@ -420,7 +420,7 @@ bool selectLockingEnabled = false;
 
 
 unsigned int texture_24[NUMTEXTURES_24];
-unsigned int texture_24[NUMTEXTURES_24];
+unsigned int texture_32[NUMTEXTURES_32];
 
 
 
@@ -428,6 +428,7 @@ unsigned int numAnimFrames;
 unsigned int currFrame;
 
 std::map<std::string, int> textureMap_24;
+std::map<std::string, int> textureMap_32;
 
 
 
